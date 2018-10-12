55b90ebdb6714ffd0b13355512c9038fcfbbd3ae
barbaradecares <barbaradecares@hotmail.com>
2018-10-12 08:33:55 -0500

    Inheritances

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student < User
+    
+    def initialize 
+       @knowledge = []
+    end 
 
+    def learn(content)
+        @knowledge << content
+    end 
+
+    def knowledge
+        @knowledge
+    end 
 end
